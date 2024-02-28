514d41c7968e0be7bf6cb2bc059f7d862ff38a07
baronvonpurr <robinhood.svensson@hotmail.com>
2024-02-28 09:57:40 +0100

    Add project files.

---
+++ Metoder1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Metoder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Skriv in ett tal");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Summan är: " + Addera(tal[0], tal[1], tal[2]));
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
